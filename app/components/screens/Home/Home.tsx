b3bbe941555cfbd3a1817efcdfc551859d78f784
Valeriy Krivonos <jeff22534@gmail.com>
2022-12-24 17:46:31 +0100

    third commit

--- app/components/screens/Home/Home.tsx
+++ app/components/screens/Home/Home.tsx
@@ -1,21 +1,85 @@
+import {
+	motion,
+	useMotionValue,
+	useScroll,
+	useSpring,
+	useTransform
+} from 'framer-motion'
 import Head from 'next/head'
-import { FC } from 'react'
+import { FC, useRef, useState } from 'react'
 import { IPost } from '../../../interfaces/post.interface'
 import PostItem from '../../../ui/post-item/PostItem'
+import styles from '../../../ui/post-item/PostItem.module.scss'
 
 export const API_URL = process.env.API_URL
+export const tabs = ['tomato', 'lettuce', 'cheese']
 
 const Home: FC<{ posts: IPost[] }> = ({ posts }) => {
+	const [selectedTab, setSelectedTab] = useState(tabs[0])
+
+	const sss = useMotionValue(700)
+
+	const ref = useRef(null)
+	const { scrollY } = useScroll({})
+	const rotate = useTransform(scrollY, [0, 1000], [0, 360])
+	const y = useTransform(scrollY, [1000, 1500], [1000, 1500])
+	const y2 = useTransform(scrollY, [0, 500], [400, 0])
+	const y3 = useTransform(scrollY, [0, 700], [500, 0])
+	const y4 = useTransform(scrollY, [0, 300], [450, 0])
+	const opacity = useTransform(scrollY, [0, 500, 700], [1, 0, 1])
+
+	const { scrollYProgress } = useScroll()
+	const scaleX = useSpring(scrollYProgress, {
+		stiffness: 100,
+		damping: 30,
+		restDelta: 0.001
+	})
+
+	/* 	const backgroundColor = useTransform(
+		scrollYProgress,
+		[0, 1],
+		['#f00', '#00f']
+	) */
+
 	return (
-		<div>
+		<motion.div ref={ref} /* style={{ backgroundColor }} */>
 			<Head>
 				<title>Home</title>
 			</Head>
-			<div>Home page:</div>
+			<motion.div>Home page:</motion.div>
+			<motion.div
+				className={styles.box}
+				transition={{
+					ease: 'easeInOut'
+				}}
+				style={{ y, rotate, opacity }}
+			></motion.div>
+			<motion.div
+				className={styles.box}
+				transition={{
+					ease: 'easeInOut'
+				}}
+				style={{ y: y2 }}
+			></motion.div>
+			<motion.div
+				className={styles.box}
+				transition={{
+					ease: 'easeInOut'
+				}}
+				style={{ y: y3, x: 400 }}
+			></motion.div>
+			<motion.div
+				className={styles.box}
+				transition={{
+					ease: 'easeInOut'
+				}}
+				style={{ y: y4, x: -250 }}
+			></motion.div>
+			<motion.div className={styles.progress} style={{ scaleX }} />
 			{posts.map(post => (
 				<PostItem post={post} key={post.id} />
 			))}
-		</div>
+		</motion.div>
 	)
 }
 
--- app/ui/post-item/PostItem.tsx
+++ app/ui/post-item/PostItem.tsx
@@ -1,14 +1,36 @@
+import { motion, Variants } from 'framer-motion'
 import Link from 'next/link'
 import { FC } from 'react'
 import { IPost } from '../../interfaces/post.interface'
 
 const PostItem: FC<{ post: IPost }> = ({ post }) => {
+	const postVariants: Variants = {
+		offscreen: {
+			opacity: 0
+		},
+		onscreen: {
+			opacity: 1,
+			transition: {
+				type: 'spring',
+				bounce: 0.4,
+				duration: 2
+			}
+		}
+	}
+
 	return (
-		<div>
-			<Link href={`/posts/${post.id}`}>{post.id}</Link>
-			<div>{post.title}</div>
-			<div>{post.body}</div>
-		</div>
+		<>
+			<motion.div
+				variants={postVariants}
+				initial="offscreen"
+				whileInView="onscreen"
+				viewport={{ once: true, amount: 'some' }}
+			>
+				<Link href={`/posts/${post.id}`}>{post.id}</Link>
+				<div>{post.title}</div>
+				<div>{post.body}</div>
+			</motion.div>
+		</>
 	)
 }
 
